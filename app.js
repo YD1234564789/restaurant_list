4a5a767e586bd8279bf0c6a73d4157abb57640cd
Sun <67455167+YD1234564789@users.noreply.github.com>
2023-03-02 02:32:39 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+const port = 3000
+
+// express template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// routes setting...
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurants_id', (req, res) => {
+  const restaurant = restaurantList.results.find(function(restaurant) {
+    return restaurant.id.toString() === req.params.restaurants_id
+  })
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter((restaurant) => {
+    return restaurant.name.toLowerCase().includes(req.query.keyword.toLowerCase())
+  })
+  res.render('index', { keyword: keyword, restaurants: restaurants })
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
